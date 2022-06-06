b011486446fb55adc01055f52f68d81d6dc81705
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-06-06 11:05:09 +0100

    Tidy up a bit.

--- smg/mediapipe/object_detector_3d.py
+++ smg/mediapipe/object_detector_3d.py
@@ -2,8 +2,9 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
+# noinspection PyPep8Naming
 from scipy.spatial.transform import Rotation as R
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 from smg.rigging.cameras import SimpleCamera
 from smg.rigging.helpers import CameraPoseConverter
@@ -33,12 +34,21 @@ class ObjectDetector3D:
 
     # CONSTRUCTOR
 
-    def __init__(self, *, image_size: Tuple[int, int], intrinsics: Tuple[float, float, float, float]):
+    def __init__(self, *, debug: bool = True, image_size: Tuple[int, int],
+                 intrinsics: Tuple[float, float, float, float]):
+        """
+        TODO
+
+        :param debug:       Whether or not to enable debugging.
+        :param image_size:  TODO
+        :param intrinsics:  TODO
+        """
         # TODO: Comment here.
+        self.__debug: bool = debug
         self.__intrinsics: Tuple[float, float, float, float] = intrinsics
-        fx, fy, cx, cy = intrinsics
 
         # TODO: Comment here.
+        fx, fy, cx, cy = intrinsics
         self.__objectron: mp.solutions.objectron.Objectron = mp.solutions.objectron.Objectron(
             static_image_mode=True,
             min_detection_confidence=0.5,
@@ -50,7 +60,8 @@ class ObjectDetector3D:
 
     # PUBLIC METHODS
 
-    def detect_objects(self, image: np.ndarray, world_from_camera: np.ndarray) -> List[Object3D]:
+    def detect_objects(self, image: np.ndarray, world_from_camera: np.ndarray, *, angle_threshold: float = 10.0,
+                       ground_plane: Tuple[float, float, float, float] = (0.0, -1.0, 0.0, 0.0)) -> List[Object3D]:
         # TODO
         objects: List[ObjectDetector3D.Object3D] = []
 
@@ -58,7 +69,9 @@ class ObjectDetector3D:
         results = self.__objectron.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
         annotated_image = image.copy()
+        # noinspection PyUnresolvedReferences
         if results.detected_objects is not None:
+            # noinspection PyUnresolvedReferences
             for detected_object in results.detected_objects:
                 mp.solutions.drawing_utils.draw_landmarks(
                     annotated_image, detected_object.landmarks_2d, mp.solutions.objectron.BOX_CONNECTIONS
@@ -70,12 +83,8 @@ class ObjectDetector3D:
                 camera_landmarks_3d: List[np.ndarray] = []
                 landmarks_3d: List[np.ndarray] = []
 
-                m: np.ndarray = np.eye(4)
-                m[0:3, 0:3] = R.from_rotvec(np.array([1, 0, 0]) * np.pi).as_matrix()
-                world_from_camera = world_from_camera @ m
-
                 for landmark_3d in detected_object.landmarks_3d.landmark:
-                    camera_landmark_3d: np.ndarray = np.array([landmark_3d.x, landmark_3d.y, landmark_3d.z])
+                    camera_landmark_3d: np.ndarray = np.array([landmark_3d.x, -landmark_3d.y, -landmark_3d.z])
                     camera_landmarks_3d.append(camera_landmark_3d)
                     landmarks_3d.append(
                         GeometryUtil.apply_rigid_transform(world_from_camera, camera_landmark_3d)
@@ -84,23 +93,27 @@ class ObjectDetector3D:
                 cam: SimpleCamera = CameraPoseConverter.pose_to_camera(np.linalg.inv(world_from_camera))
                 for i in [1, 2, 5, 6]:
                     landmarks_3d[i] = GeometryUtil.find_plane_intersection(
-                        cam.p(), landmarks_3d[i] - cam.p(), (0, -1, 0, 0)
+                        cam.p(), landmarks_3d[i] - cam.p(), ground_plane
                     )
                     landmarks_3d[i + 2] = landmarks_3d[i] + np.array([0, -1, 0])
 
-                landmarks_3d[0] = (landmarks_3d[1] + landmarks_3d[2] + landmarks_3d[5] + landmarks_3d[6]) / 4 + np.array([0, -0.5, 0])
+                landmarks_3d[0] = np.mean([landmarks_3d[i] for i in [1, 2, 5, 6]], axis=0) + np.array([0, -0.5, 0])
 
-                v1: np.ndarray = landmarks_3d[2] - landmarks_3d[1]
-                v2: np.ndarray = landmarks_3d[5] - landmarks_3d[1]
-                v3: np.ndarray = landmarks_3d[2] - landmarks_3d[6]
-                v4: np.ndarray = landmarks_3d[5] - landmarks_3d[6]
-                angle1: float = np.arccos(np.clip(np.dot(v1, v2), -1.0, 1.0)) * 180 / np.pi
-                angle2: float = np.arccos(np.clip(np.dot(v3, v4), -1.0, 1.0)) * 180 / np.pi
-                if np.fabs(angle1 - 90) <= 2.0 and np.fabs(angle2 - 90) <= 2.0:
+                angle1: Optional[float] = (180 / np.pi) * GeometryUtil.angle_between(
+                    landmarks_3d[2] - landmarks_3d[1], landmarks_3d[5] - landmarks_3d[1]
+                )
+                angle2: Optional[float] = (180 / np.pi) * GeometryUtil.angle_between(
+                    landmarks_3d[2] - landmarks_3d[6], landmarks_3d[5] - landmarks_3d[6]
+                )
+                if self.__debug:
+                    print(angle1, angle2)
+                if angle1 is not None and angle2 is not None \
+                        and np.fabs(angle1 - 90) <= angle_threshold and np.fabs(angle2 - 90) <= angle_threshold:
                     objects.append(ObjectDetector3D.Object3D(landmarks_3d))
 
-        cv2.imshow("Annotated Image", cv2.resize(annotated_image, (640, 480)))
-        cv2.waitKey(1)
+        if self.__debug:
+            cv2.imshow("Annotated Image", cv2.resize(annotated_image, (640, 480)))
+            cv2.waitKey(1)
 
         # TODO
         return objects
